0eabf3fb488b9eb7236680ce1fcc4cdcece80085
Eric Iacutone <eric.iacutone@gmail.com>
2019-03-21 17:30:20 -0400

    Clean up notes bash script

--- notes.sh
+++ notes.sh
@@ -1,36 +1,33 @@
 #!/bin/bash
 set -e
 
-fpath=$HOME/notes.md
+fpath=$HOME/Dropbox/notes.md
 
 if [ "$1" == "cat" ]; then
   cat "$fpath"
   exit 0
-elif [ "$1" == "rg" ]; then
-  rg "$2" "$fpath"
-elif [ "$1" == "nano" ]; then
-  nano "$fpath"
+elif [ "$1" == "ag" ]; then
+  ag "$2" "$fpath"
+elif [ "$1" == "nvim" ]; then
+  nvim "$fpath"
 elif [ "$1" == "--help" ]; then
   printf 'Commands: \n-----------------------------------------------\n
   $ notes \n
-  $ notes --help\t\t--\tdisplay this help\n
+  $ notes --help\t--\tdisplay this help\n
   $ notes date\t\t--\tadd date row to notes\n
-  $ notes <text>\t\t--\tadd new entry \n
+  $ notes <text>\t--\tadd new entry \n
   $ notes cat\t\t--\tprint notes using cat\n
-  $ notes rg <pattern>\t--\tripgrep notes\n
-  Remember to use #tags (for easier grepping)!\n\n'
+  $ notes ag <pattern>\t--\grep notes\n'
 elif [ "$1" == "date" ]; then
   {
     echo ''
     echo '# '"$(date +"%m-%d-%Y-%T")"
     echo '-'
   } >> "$fpath"
-elif [ "$1" == "" ]; then
-  less +G "$fpath"
 else
   {
     echo ''
     echo "$@"
     echo ''
-  } >>"$fpath"
+  } >> "$fpath"
 fi
